33bf9c5bd9a9a3e73d623d4b3c109bd6af420280
Devin de Spindler <devin.vipxxx@gmail.com>
2024-04-17 08:01:40 +0200

    Initial commit

---
+++ src/test/java/ch/bbw/jr/projekt/ProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.jr.projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
