c48c3f2fad426fbc4a4be6f5a00ed743465667a6
userjann <jann.reutegger@lernende.bbw.ch>
2024-05-22 21:45:19 +0200

    Startseite erstellt error.html erstellt html und css dateien vom templates in static ordner verschoben styleIndex.css erstellt

--- src/main/java/ch/bbw/jr/mitglied/MitgliedController.java
+++ src/main/java/ch/bbw/jr/mitglied/MitgliedController.java
@@ -3,13 +3,10 @@ package ch.bbw.jr.mitglied;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MitgliedController {
@@ -35,12 +32,4 @@ public class MitgliedController {
         return "redirect:/showMember"; // Korrekte Weiterleitung
     }
 
-    @ExceptionHandler(Exception.class)
-    public ModelAndView handleError(HttpServletRequest req, Exception ex) {
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("exception", ex);
-        mav.addObject("url", req.getRequestURL());
-        mav.setViewName("error");
-        return mav;
-    }
 }
