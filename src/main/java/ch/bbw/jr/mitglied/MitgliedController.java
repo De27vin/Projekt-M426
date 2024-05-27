85ed0a59c4fd758e1f712751d53a1d585c7ff2a8
userjann <jann.reutegger@lernende.bbw.ch>
2024-05-27 13:01:50 +0200

    Mitglieder hinzufügen fertig addMemberStyles.css erstellt für addMember.html

--- src/main/java/ch/bbw/jr/mitglied/MitgliedController.java
+++ src/main/java/ch/bbw/jr/mitglied/MitgliedController.java
@@ -1,41 +1,49 @@
-    package ch.bbw.jr.mitglied;
+package ch.bbw.jr.mitglied;
 
-    import org.springframework.beans.factory.annotation.Autowired;
-    import org.springframework.stereotype.Controller;
-    import org.springframework.ui.Model;
-    import org.springframework.web.bind.annotation.GetMapping;
-    import org.springframework.web.bind.annotation.ModelAttribute;
-    import org.springframework.web.bind.annotation.PostMapping;
-    import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-    import java.lang.reflect.Member;
-    import java.time.LocalDate;
-    import org.springframework.format.annotation.DateTimeFormat;
-    @Controller
-    public class MitgliedController {
+import java.time.LocalDate;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.validation.BindingResult;
 
-        @Autowired
-        private MitgliedService mitgliedService;
+@Controller
+public class MitgliedController {
 
-        @GetMapping("/showMember")
-        public String showMembers(Model model) {
-            model.addAttribute("members", mitgliedService.getMemberList());
-            return "showMember";
-        }
+    @Autowired
+    private MitgliedService mitgliedService;
 
-
-        @GetMapping("/addMember")
-        public String showAddMemberForm(Model model) {
-            Mitglied newMember = new Mitglied();
-            newMember.setEntryDate(LocalDate.now());
-            model.addAttribute("newMember", newMember);
-            return "addMember   ";
-        }
-
-
-        @GetMapping("/ergebnissFilter")
-        public String filterMembers(@RequestParam("name") String name, Model model) {
-            model.addAttribute("members", mitgliedService.findMembersByName(name));
-            return "ergebnissFilter"; // Dies zeigt die Vorlage "filterMembers.html" an
-        }
+    @GetMapping("/showMember")
+    public String showMembers(Model model) {
+        model.addAttribute("members", mitgliedService.getMemberList());
+        return "showMember";
     }
+
+    @GetMapping("/addMember")
+    public String showAddMemberForm(Model model) {
+        Mitglied newMember = new Mitglied();
+        newMember.setEntryDate(LocalDate.now());
+        model.addAttribute("newMember", newMember);
+        return "addMember";
+    }
+
+    @PostMapping("/addMember")
+    public String addMember(@ModelAttribute("newMember") Mitglied newMember, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "addMember";
+        }
+        mitgliedService.createMember(newMember);
+        return "redirect:/showMember";
+    }
+
+    @GetMapping("/ergebnissFilter")
+    public String filterMembers(@RequestParam("name") String name, Model model) {
+        model.addAttribute("members", mitgliedService.findMembersByName(name));
+        return "ergebnissFilter";
+    }
+}
